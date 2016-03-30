e23e0d7c4b3d3164c777f9ef57229a542f3cd0d0
Mateusz Kuzak <mateusz.kuzak@gmail.com>
2016-03-30 19:13:57 +0200

    Add xAxis

--- vis.js
+++ vis.js
@@ -4,25 +4,21 @@ d3.json("data/patientoutput.json", function(patientStats) {
   var h = 500;
   var padding = 20;
 
-  // make scale maping for positioning tests on x axis
+  // scales
+
+  // for x need to translate test name to number
   var testnames = patientStats.map(function(t) {
     return t.testnames;
   });
   testnames = _.union(testnames);
-  var testUnit = Math.floor(w / testnames.length);
-  var testScale = []
-  for (var i = 0; i <= testnames.length; i++) {
-    testScale.push(i * testUnit + 25)
-  }
-  var testMap = {};
-  for (var i = 0; i < testnames.length; i++) {
-    testMap[testnames[i]] = testScale[i];
-  }
 
-  // make y axis scale for test scores
+  var xScale = d3.scale.linear()
+                       .domain([0, testnames.length - 1])
+                       .range([padding, w - padding])
+
   var yScale = d3.scale.linear()
-                               .domain([-10, 10])
-                               .range([padding, w - padding]);
+                       .domain([-5, 5])
+                       .range([h - padding, padding]);
 
 
 
@@ -33,10 +29,27 @@ d3.json("data/patientoutput.json", function(patientStats) {
           .attr("height", h);
 
   // define axes
+  var xAxis = d3.svg.axis()
+                    .scale(xScale)
+                    .orient("bottom")
+                    .tickValues(d3.range(testnames.length))
+                    .tickFormat(function(t) {
+                      return testnames[t];
+                    });
+  svg.append("g")
+     .attr("class", "axis")
+     .call(xAxis)
+   .selectAll("text")
+     .attr("dx", "3em")
+     .attr("dy", "3em")
+     .attr("transform", "rotate(45)");
+
+
   var yAxis = d3.svg.axis()
                     .scale(yScale)
                     .orient("right");
   svg.append("g")
+     .attr("class", "axis")
      .call(yAxis);
 
 // add 'scatterplot' elements
@@ -45,7 +58,7 @@ d3.json("data/patientoutput.json", function(patientStats) {
      .enter()
      .append("circle")
      .attr("cx", function(d) {
-       return testMap[d['testnames']];
+       return xScale(testnames.indexOf(d['testnames']));
      })
      .attr("cy", function(d) {
        return yScale(d['univariateT']);
